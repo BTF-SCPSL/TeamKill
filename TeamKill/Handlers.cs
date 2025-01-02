09259171a54b5c42bee45f20df968f784ddddd92
RuslanSamburov <samburov.com@gmail.com>
2025-01-02 20:15:37 +0300

    Добавьте файлы проекта.

---
+++ TeamKill/Handlers.cs
@@ -0,0 +1,44 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Map;
+using Exiled.Events.EventArgs.Player;
+
+namespace TeamKill
+{
+	public sealed class Handlers
+	{
+		public void OnShooting(ShootingEventArgs ev)
+		{
+			if (Server.FriendlyFire)
+			{
+				ev.IsAllowed = true;
+			} else
+			{
+				if (ev.Player.LeadingTeam == ev.ClaimedTarget.LeadingTeam)
+				{
+					ev.IsAllowed = false;
+				}
+			}
+		}
+
+		public void OnExplodingGrenade(ExplodingGrenadeEventArgs ev)
+		{
+			foreach (Player player in ev.TargetsToAffect.ToList())
+			{
+				if (Server.FriendlyFire)
+				{
+					break;
+				}
+
+				if (ev.Player == player)
+				{
+					continue;
+				}
+
+				if (ev.Player.LeadingTeam == player.LeadingTeam)
+				{
+					ev.TargetsToAffect.Remove(player);
+				}
+			}
+		}
+	}
+}
---
+++ TeamKill/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using Exiled.API.Features;
+using TeamKill.Configs;
+
+namespace TeamKill
+{
+    public class Plugin : Plugin<Config>
+    {
+		public override string Author => "Руслан0308c";
+		public override string Name => "TeamKill";
+		public override Version RequiredExiledVersion => new(9, 2, 1);
+		public override Version Version => new(1, 0, 0);
+
+		public Plugin? Singleton { get; private set; }
+		public Handlers? _handlers { get; private set; }
+
+		public override void OnEnabled()
+		{
+			_handlers = new();
+			Singleton = this;
+
+			Exiled.Events.Handlers.Player.Shooting += _handlers.OnShooting;
+			Exiled.Events.Handlers.Map.ExplodingGrenade += _handlers.OnExplodingGrenade;
+
+			base.OnEnabled();
+		}
+
+		public override void OnDisabled()
+		{
+			Exiled.Events.Handlers.Player.Shooting -= _handlers.OnShooting;
+			Exiled.Events.Handlers.Map.ExplodingGrenade -= _handlers.OnExplodingGrenade;
+
+			Singleton = null;
+			_handlers = null;
+
+			base.OnDisabled();
+		}
+	}
+}
